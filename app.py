aa5b5a2b5e5d9ed7aa26268ac3b5dce1e2fae85b
Taha Çinkılıç <tahacinkilic@icloud.com>
2022-09-29 19:35:52 +0300

    RESTful API for titanic survival prediction

---
+++ app.py
@@ -0,0 +1,47 @@
+import titanic_prediction as tp
+from joblib import dump, load
+from flask import Flask, request
+from flask_restful import Resource, Api
+from requests import get, post
+
+app = Flask(__name__)
+api = Api(app)
+
+values = {}
+
+class takeDict(Resource):
+    def get(self, info):
+        print(type(values))
+        return values[info]
+    def post(self, info):
+        values[info] = request.json
+        return values
+
+class train(Resource):
+    def get(self, info):
+        dump(tp.createModel(), f"{info}.model")
+        return "Successfully trained."
+
+class predict(Resource):
+    def get(self, info):
+        fitModel, embEnc, sexEnc, ageScaler = load(f"{info}.model")
+        result = tp.predRes(fitModel, embEnc, sexEnc, ageScaler, values[info])
+        return result
+
+api.add_resource(takeDict, "/<string:info>")
+api.add_resource(train, "/train/<string:info>")
+api.add_resource(predict, "/train/predict/<string:info>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+---Console Command Samples---
+python
+import json
+from requests import post, get
+post("http://127.0.0.1:5000/Taha", json = {"Pclass": "2", "Sex": "male", "Age": 23.0, "SibSp": "2", "Parch": "0", "Embarked": "S"}).json()
+get("http://127.0.0.1:5000/Taha").json()
+get("http://127.0.0.1:5000/train/Taha").json()
+get("http://127.0.0.1:5000/train/predict/Taha").json()
+"""
---
+++ titanic_prediction.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import json
+from sklearn import svm
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedKFold
+
+def createModel():
+    trainData = pd.read_csv("train.csv")
+    trainData.drop(labels = ["PassengerId", "Name", "Ticket", "Fare", "Cabin"], axis = 1, inplace = True)
+
+    ageImp = SimpleImputer().fit(trainData[["Age"]])
+    trainData["Age"] = ageImp.transform(trainData[["Age"]])
+
+    embImp = SimpleImputer(strategy = "most_frequent").fit(trainData[["Embarked"]])
+    trainData["Embarked"] = embImp.transform(trainData[["Embarked"]])
+
+    embEnc = OrdinalEncoder().fit(trainData[["Embarked"]])
+    trainData["Embarked"] = embEnc.transform(trainData[["Embarked"]])
+
+    sexEnc = OrdinalEncoder().fit(trainData[["Sex"]])
+    trainData["Sex"] = sexEnc.transform(trainData[["Sex"]])
+
+    ageScaler = StandardScaler().fit(trainData[["Age"]])
+    trainData["Age"] = ageScaler.transform(trainData[["Age"]])
+
+    y = trainData.pop("Survived")
+    X = trainData[trainData.columns]
+
+    parameters = [{"kernel":["linear", "rbf", "sigmoid"], "C":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}]
+    rkfold = RepeatedKFold(n_splits = 10, n_repeats = 1, random_state = 12883823)
+    clf = GridSearchCV(estimator = svm.SVC(), param_grid = parameters, scoring = "accuracy", refit = True, cv = rkfold)
+    fitModel = clf.fit(X, y)
+    return fitModel, embEnc, sexEnc, ageScaler
+
+def predRes(fitModel, embEnc, sexEnc, ageScaler, values):
+    framed = pd.DataFrame(data = values, index = [1])
+    framed["Embarked"] = embEnc.transform(framed[["Embarked"]])
+    framed["Sex"] = sexEnc.transform(framed[["Sex"]])
+    framed["Age"] = ageScaler.transform(framed[["Age"]])
+    pred = fitModel.predict(framed.to_dict("split")["data"]).tolist()[0]
+    result = {"Prediction" : pred}
+    return json.dumps(result)
